90bbefc4813e7f175f2b6cb92d471a1c92755db0
OmarMWarraich <earthling986@gmail.com>
2024-06-22 17:21:54 +0500

    feat: Registration Cntrlr, Helpers, SignUp Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  def login(user)
+    Current.user = user
+    reset_session
+    session[:user_id] = user.id
+  end
+
+  def logout(_user)
+    Current.user = nil
+    reset_session
+  end
+
+  def current_user
+    Current.user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
 end
