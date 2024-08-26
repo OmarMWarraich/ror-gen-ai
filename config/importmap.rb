f3fc14807166f7978c1bd8a2b7c1a89a7987e8f9
OmarMWarraich <earthling986@gmail.com>
2024-08-26 22:11:56 +0500

    fix: Bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,8 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-pin "@popperjs/core/lib/index.js", to: "@popperjs--core--lib--index.js.js" # @2.11.8
-pin_all_from "@popperjs/core/lib", under: "@popperjs/core/lib"
+
 pin "bootstrap" # @5.3.3
-pin "@popperjs/core/lib", to: "@popperjs--core--lib.js" # @2.11.8
